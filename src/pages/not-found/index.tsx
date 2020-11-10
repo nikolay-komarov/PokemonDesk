5873dfce008548676fe9d641a5fec3df93f53347
nikolay-komarov <kny.komarov@gmail.com>
2020-11-10 19:22:27 +0300

    добавит стилизацию для страницы 404 (NotFoundPage)

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { navigate } from 'hookrouter';
 
+import s from './not-found.module.scss';
+
+import TeamRocket from './assets/team-rocket.png';
+
 import Button, { ButtonSize, ButtonColor } from '../../components/button';
 
 const NotFoundPage = () => {
   return (
-    <div>
-      This is NotFound Page
-      <Button size={ButtonSize.medium} color={ButtonColor.yellow} isFullWidth={false} onClick={() => navigate('/')}>
-        Return
-      </Button>
+    <div className={s.root}>
+      <div className={s.wrap}>
+        <div className={s.backText}>404</div>
+        <div className={s.wrapContent}>
+          <img src={TeamRocket} alt="Team Rocket" />
+          <div className={s.textContent}>
+            <span>The rocket team</span> has won this time
+          </div>
+        </div>
+        <Button size={ButtonSize.medium} color={ButtonColor.yellow} isFullWidth={false} onClick={() => navigate('/')}>
+          Return
+        </Button>
+      </div>
     </div>
   );
 };
